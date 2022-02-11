cf04d5633518bb912a63cd9084bee59f22b138d8
islanderian <createdby.island@gmail.com>
2022-02-11 22:47:54 +0900

    #0.8 - Blockchain Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
-// interface 대신 객체의 타입 정의
-class Human {
-  public name: string;
-  private age: number; // private 로 선언하면 Human 클래스 밖에서는 해당 변수를 사용할 수 없음
-  public gender: string;
-  // 클래스가 시작할때마다 호출되는 메소드
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const lynn = new Human("Lynn", 18, "female");
+const genesisBlock: Block = new Block(0, "2020200202", "", "Hello", 123456);
 
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayHi(lynn));
-export {}; //typescript에서는 이걸 붙여줘야 한다. 모듈이라는 것을 알려주기 위해
+console.log(blockchain);
+
+export {};
