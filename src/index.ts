7d20be59d826e678630f6162df681149b3c32ed9
islanderian <createdby.island@gmail.com>
2022-02-11 22:36:06 +0900

    #0.7 - Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-// 객체의 타입 정의
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+// interface 대신 객체의 타입 정의
+class Human {
+  public name: string;
+  private age: number; // private 로 선언하면 Human 클래스 밖에서는 해당 변수를 사용할 수 없음
+  public gender: string;
+  // 클래스가 시작할때마다 호출되는 메소드
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
-// 객체
-const person = {
-  name: "nicolas",
-  age: 22,
-  gender: "male",
-};
+
+const lynn = new Human("Lynn", 18, "female");
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 export {}; //typescript에서는 이걸 붙여줘야 한다. 모듈이라는 것을 알려주기 위해
