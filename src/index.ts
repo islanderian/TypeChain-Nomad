2d654b18e3e56d41c026866e0809f95877671a6b
islanderian <createdby.island@gmail.com>
2022-02-11 23:09:41 +0900

    #0.9 - CryptoJS 설치

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
+// TypeScript 에서의 import
+import * as CryptoJS from "crypto-js"; // 해쉬 함수를 통해 암호하를 할 수 있도록 해주는 패키지
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  // static method, 클래스가 생성되지 않았어도 클래스 안에서 호출할 수 있는 method
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string => {
+    return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+  };
 
   constructor(
     index: number,
@@ -22,8 +35,15 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "2020200202", "", "Hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain);
+// Block 객체들의 전체 배열을 리턴
+const getBlockchain = (): Block[] => blockchain;
+// 가장 최근의 Block 객체를 리턴
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+console.log(getNewTimeStamp());
 
 export {};
