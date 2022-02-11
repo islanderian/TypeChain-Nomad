7f630c945ded1f49ceae8bf3a0ba11ed5d8dcd8e
islanderian <createdby.island@gmail.com>
2022-02-11 22:24:43 +0900

    #0.6 - Interfaces on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+// 객체의 타입 정의
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+// 객체
+const person = {
+  name: "nicolas",
+  age: 22,
+  gender: "male",
 };
 
-console.log(sayHi("Nicolas", 24, "male"));
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+
+console.log(sayHi(person));
 export {}; //typescript에서는 이걸 붙여줘야 한다. 모듈이라는 것을 알려주기 위해
